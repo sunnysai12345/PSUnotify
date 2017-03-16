8028eea6d854677245b02fa557765e610585ccc3
sunnysai12345 <sunnysai12345@gmail.com>
2017-03-16 18:35:30 +0530

    Modified Code
    Removed errors encountered while running the program

--- psunotify.py
+++ psunotify.py
@@ -5,28 +5,33 @@ from random import *
 br=mechanize.Browser()
 br.addheaders = [('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')]
 br.set_handle_robots(False)
+#For page exploration
 page=raw_input('Enter Page No:')
-print type(page)
-p=urllib2.Request('https://www.google.co.in/search?q=gate+psu+2016+ext:pdf&start='+page)
+#print type(page)
+p=urllib2.Request('https://www.google.co.in/search?q=gate+psu+2017+ext:pdf&start='+page)
 ht=br.open(p)
 text='<cite\sclass="_Rm">(.+?)</cite>'
 patt=re.compile(text)
 h=ht.read()
-url=re.findall(patt,h)
+urls=re.findall(patt,h)
 int=0
-while int<len(url):
-    url[int]=url[int].replace("<b>","")
-    url[int]=url[int].replace("</b>","")
+while int<len(urls):
+    urls[int]=urls[int].replace("<b>","")
+    urls[int]=urls[int].replace("</b>","")
     int=int+1
-print url
-for i in url:
+print urls
+for url in urls:
     try:
-     q=str(randint(0,1000))
-     r=urllib2.urlopen("http://"+i)
+     temp=url.split("/")
+     q=temp[len(temp)-1]
+     if "http" in url:
+         r=urllib2.urlopen(url)
+     else:
+         r=urllib2.urlopen("http://"+url)
      file=open('psu2'+q+'.pdf','wb')
      file.write(r.read())
      file.close()
      print "Done"
     except urllib2.URLError as e:
-     print "Sorry there exists a problem with this URL Please Download this Manually "%i
+     print "Sorry there exists a problem with this URL Please Download this Manually "+str(url)
 
